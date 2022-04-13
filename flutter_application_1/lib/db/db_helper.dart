5954326b2519a53a4d16dc4c6d9296d7b7068e36
ossamaDevloper <ossama.Devloper@Movic.com>
2022-04-13 15:07:34 +0000

    SolveDBProblem

--- flutter_application_1/lib/db/db_helper.dart
+++ flutter_application_1/lib/db/db_helper.dart
@@ -16,14 +16,7 @@ class DBHelper {
         String path = await getDatabasesPath() + 'task.db';
         db = await openDatabase(path, version: version,
             onCreate: (Database db2, int version2) async {
-         
-          await db?.execute('CREATE TABLE $_tabelName('
-              'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-              'title STRING ,note TEXT, date STRING,'
-              'startTime STRING, endTime STRING ,'
-              'remind INTEGER, repeat STRING, '
-              'color INTEGER,'
-              'isComplted INTEGER)');
+          await db?.execute('CREATE TABLE $_tabelName(id INTEGER PRIMARY KEY AUTOINCREMENT,title STRING ,note TEXT, date STRING,startTime STRING, endTime STRING ,remind INTEGER, repeat STRING, color INTEGER,isComplted INTEGER');
                debugPrint('The DB Was Created');
         });
       } catch (e) {
